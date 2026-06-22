35c663524f1cb3a700e2efaccf2c3d34e877c7a2
phuchoangvo210405 <phuchoangvo210405@gmail.com>
2026-06-23 00:03:45 +0700

    TASK-120 Xây dựng MedicineController (API Tra cứu & Chi tiết thuốc)

---
+++ PharmaDicBackEnd/PharmaDicBackEnd.ApiService/DTOs/MedicineDetailDto.cs
@@ -0,0 +1,24 @@
+﻿namespace PharmaDicBackEnd.ApiService.DTOs
+{
+    public class MedicineDetailDto
+    {
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; } = null!;
+        public string? CategoryName { get; set; }
+        public string? DosageForm { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Uses { get; set; }
+        public string? Dosage { get; set; }
+        public string? Contraindications { get; set; }
+        public string? SideEffects { get; set; }
+
+        
+        public List<IngredientDto> Ingredients { get; set; } = new List<IngredientDto>();
+    }
+
+    public class IngredientDto
+    {
+        public string IngredientName { get; set; } = null!;
+        public string? Amount { get; set; } // Hàm lượng (VD: 500mg)
+    }
+}
